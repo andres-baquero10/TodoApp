164618d628272f77d8dd9509cbb530fe97085204
Andrés Baquero <andresbaqueroj7@gmail.com>
2020-07-02 14:02:49 -0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TodoApp from "./components/TodoApp/TodoApp";
+import "./App.scss";
+
+function App() {
+  return <TodoApp />;
+}
+
+export default App;
---
+++ src/components/TodoApp/TodoApp.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+import * as React from "react";
+import { useState } from "react";
+
+export interface TodoAppProps {}
+
+const TodoApp: React.SFC<TodoAppProps> = () => {
+  return <div className="TodoApp-container"></div>;
+};
+
+export default TodoApp;
